08430de8c1286139796e83dac5512c052b9322cd
repl.it user <replituser@example.com>
2020-10-01 15:07:37 +0000

    where i left off

---
+++ basics/input/data_types.py
@@ -0,0 +1,15 @@
+print("What is your name human?")
+name = input()
+
+print("What is your age?")
+age = int(input())
+
+print ("How tall are you (in meters)?")
+height = int(input()
+
+print ("How much do you weigh?")
+weight= int(input))
+
+bmi= height**-weight
+
+print (name +"you are" ,age +"years old and you bmi is" + bmi)
