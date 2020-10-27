19340568527ff725212292028ac0420cc7d975f7
repl.it user <replituser@example.com>
2020-10-27 10:49:09 +0000

    First if statement

---
+++ basics/decisions/if.py
@@ -0,0 +1,7 @@
+print("What type of book is this?")
+book_type = input()
+#Determine if the book is an adventure book
+if (book_type == "adventure"):
+  print("I like adventure books!")
+  
+print ("Finished reading book.")
