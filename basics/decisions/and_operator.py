0eaa839124618837877c20e95a6be6996b594dcf
repl.it user <replituser@example.com>
2020-10-27 14:19:22 +0000

    Added code to demonstrate the use of a simple while loop.

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,32 @@
+print("What did I hear?")
+heard = input()
+print("What did I see?")
+saw = input()
+
+if ( (heard == "grr") and (saw == "two red eyes") ):
+  print("There is a scary creature.\nI should get out of here!")
+else:
+  print("I am a little scared but I will continue.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,39 @@
+print("How many cable should I remove?")
+cables_to_remove = int(input())
+
+cables_removed = 0
+
+
+while (cables_removed < cables_to_remove):
+  print("Removed cable.")
+  cables_removed += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
