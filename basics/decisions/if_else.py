2d06081cf95e0ca41cb47e29db8d20ced28b2392
repl.it user <replituser@example.com>
2020-10-27 10:56:36 +0000

    Added code to demonstarte the use fo an if...else statent..

---
+++ basics/decisions/if_else.py
@@ -0,0 +1,23 @@
+print("Plese enter activity to be performed.")
+activity = input()
+
+if (activity == "calculate"):
+  print("Performing calculations...")
+
+else:
+  print("Performing activity...")
+
+print("Acrivity completed!")
+
+
+
+
+
+
+
+
+
+
+
+
+
