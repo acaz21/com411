07eff443bad8155964ae14468c4eca77664e2458
repl.it user <replituser@example.com>
2020-11-08 14:17:01 +0000

    Added code to demonstrated the use of one loop neseted in another

---
+++ basics/repetitions/nested_loop/nested.py
@@ -0,0 +1,22 @@
+print("How many rows should I have?")
+rows = int(input())
+print("How many colums should I have?")
+columns = int(input())
+
+for row in range (0,rows,1):
+  for column in range(0,columns,1):
+    print(":(", end="")
+  print()
+print("Done!")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,20 @@
+for number in range(0, 3 , 1):
+  for number in range(0, 10 , 1):
+    print(number, "|" , end=" ")
+  print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
