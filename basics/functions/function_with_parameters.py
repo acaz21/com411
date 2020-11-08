d50e1a2868d61730cf63d7182a2995e6168a40cd
repl.it user <replituser@example.com>
2020-11-08 16:34:14 +0000

    Added code to demonstrate the use of a user-defined function with multipel parameters.

---
+++ basics/functions/function_with_parameters.py
@@ -0,0 +1,8 @@
+def climb_ladder(steps_remaining, steps_corssed):
+  if (steps_remaining > steps_corssed):
+    print("Still some way to go!")
+  else:
+    print("We are amost there!")
+
+climb_ladder(5,2)
+climb_ladder(2,5)
